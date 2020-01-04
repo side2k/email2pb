7ae7b129214065c535302208aadf4ec4d8dd5bcd
Patrick Gross <pgross41@gmail.com>
2020-01-04 00:35:32 -0600

    Parse and push images from dvr163 emails

--- email2pb.py
+++ email2pb.py
@@ -2,15 +2,16 @@ import argparse
 import base64
 import email
 import logging
+import nltk   
+import os
 import re
 import sys
 
-from pushbullet import PushbulletAPIClient
-
+from pushbullet import Pushbullet
 
 logger = logging.getLogger(__name__)
 
-
+# Read arguments
 parser = argparse.ArgumentParser(description='Send PushBullet PUSH based on email message')
 parser.add_argument(
     'infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin,
@@ -20,9 +21,8 @@ parser.add_argument('--debug', help='Enable debug mode', action='store_true')
 parser.add_argument("--debug_log", type=str)
 args = parser.parse_args()
 
+# Read stdin and write it out if needed
 stdin_data = args.infile.read()
-
-
 debug_mode = args.debug
 if debug_mode:
     logger.debug('Debug mode enabled')
@@ -34,42 +34,37 @@ if debug_mode:
         debug_log.write(stdin_data)
         debug_log.write("-------------------------\n")
 
-msg = email.message_from_string(stdin_data)
 args.infile.close()
 
-def decode_field(field_raw):
-    match = re.match(r'\=\?([^\?]+)\?([BQ])\?([^\?]+)\?\=', field_raw)
-    if match:
-        charset, encoding, field_coded = match.groups()
-        if encoding == 'B':
-            field_coded = bytearray(field_coded, encoding=charset)
-            field_coded = base64.decodestring(field_coded)
-        return field_coded.decode(charset)
-    else:
-        return field_raw
+# Parse stdin
+pb = Pushbullet(args.key)
+parts = stdin_data.split("--=======JUAN_SMTP_V1_0=======")
 
-subject_raw = msg.get('Subject', '')
-subject = decode_field(subject_raw)
+# Parse out the text
+text = parts[0]
+text = re.findall("\n\n(.*)", text)[0] # Remove header info
+text = re.sub(r"(?is)<(script|style).*?>.*?(</\1>)", "", text.strip()) # Remove style tags
+text = re.sub(r"(?s)<.*?>", " ", text).strip() # Get text content
 
-sender = decode_field(msg.get('From', ''))
+# Hardcoded: Only interested in channel 2
+if(not re.findall("Event:Motion detect in video channel 2", text)):
+    print("Not channel 2:")
+    print(text)
+    quit()
 
-body_text = ''
-for part in msg.walk():
-    if part.get_content_type() == 'text/plain':
-        body_part = part.get_payload()
-        part_encoding = part.get_content_charset()
+# Write out the base64 data to image file
+image_part = parts[1]
+file_name = re.findall("name=\"(.*?)\"", image_part)[0]
+base64_image = re.findall("\n\n(.*)", image_part)[0].strip()
+with open(file_name, "wb") as fh:
+    fh.write(base64_image.decode('base64'))
 
-        if part.get('Content-Transfer-Encoding') == 'base64':
-            body_part = bytearray(body_part, encoding=part_encoding)
-            body_part = base64.decodestring(body_part)
-            body_part = body_part.decode(part_encoding)
+# Upload the image to pushbullet
+with open(file_name, "rb") as pic:
+    file_data = pb.upload_file(pic, file_name)
 
-        if body_text:
-            body_text = '%s\n%s' % (body_text, body_part)
-        else:
-            body_text = body_part
+# Send push
+push = pb.push_file(body = text, **file_data)
 
-body_text = '%s\nFrom: %s' % (body_text, sender)
-
-client = PushbulletAPIClient(api_key=args.key)
-client.push_note_to_device(None, body_text, title=subject)
+# Delete the image
+os.remove(file_name)
